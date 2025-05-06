843e7c6f0ef3d27f8b954ebb8e862388f506d3cf
Tyler Latshaw <tylerlatshaw@gmail.com>
2025-05-06 19:19:30 -0400

    Updated words to exclude used words

---
+++ src/app/api/get-all-words/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import { WordType } from "../../lib/type-library";
+import supabase from "../../../utilities/supabase";
+
+export async function GET() {
+    try {
+        const { data } = await supabase.from("possible_words").select();
+
+        const results: WordType[] = [];
+
+        data?.forEach((row) => {
+            results.push({
+                WordleWordId: row.id,
+                Word: row.word,
+                Score: row.score
+            });
+        });
+
+        return NextResponse.json(results);
+    } catch (error) {
+        console.error("Error fetching data: ", error);
+        return new NextResponse("Error fetching data");
+    }
+}
